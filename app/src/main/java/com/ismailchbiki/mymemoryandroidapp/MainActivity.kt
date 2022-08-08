f87543e5b8418578527f89d72cb0d62ed06a2a4c
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2022-08-08 23:16:35 +0200

    Scaffolding of the MainActivity is setup

--- app/src/main/java/com/ismailchbiki/mymemoryandroidapp/MainActivity.kt
+++ app/src/main/java/com/ismailchbiki/mymemoryandroidapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.ismailchbiki.mymemoryandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
+//Every activity represents one screen in the app
 class MainActivity : AppCompatActivity() {
+
+    //lateinit:
+    // The variables will be set after onCreate method is run
+    // after being invoked by the android system,
+    // (They are not going to be created at the time of construction of the MainActivity)
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
