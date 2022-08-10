74a6f53b767ac399fde6740206ae0c40af67e75c
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2022-08-10 23:35:19 +0200

    Creating RecyclerView with Measured Views

--- app/src/main/java/com/ismailchbiki/mymemoryandroidapp/MainActivity.kt
+++ app/src/main/java/com/ismailchbiki/mymemoryandroidapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ismailchbiki.mymemoryandroidapp
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 //Every activity represents one screen in the app
@@ -23,5 +24,15 @@ class MainActivity : AppCompatActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        //The Adapter is more involved than the LayoutManager
+        //because it's responsible for taking in the underlying dataset of the RecylcerView
+        //and adapts each piece of data into a view
+        rvBoard.adapter = MemoryBoardAdapter(this, 8) //total of elements in the grid
+        rvBoard.setHasFixedSize(true) //this is optional, (for performance optimisation)
+        rvBoard.layoutManager = GridLayoutManager(
+            this, //this: represents the context which is the MainActivity
+            2 //number of columns
+        )
     }
 }
---
+++ app/src/main/java/com/ismailchbiki/mymemoryandroidapp/MemoryBoardAdapter.kt
@@ -0,0 +1,54 @@
+package com.ismailchbiki.mymemoryandroidapp
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
+
+//this class is a subclass of the RecyclerView adapter class
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+    //companion objects are singletons where we can define constants,
+    //we can access its members directly through the containing class
+    //similar to static objects in Java
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
+    //this is responsible for creating one view of the RecyclerView
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams =
+            view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = numPieces
+
+    //this is responsible of taking the data at the given position and binding it to the ViewHolder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+        fun bind(position: Int) {
+            imageButton.setOnClickListener {
+                Log.i(TAG, "Clicked on position $position")
+            }
+        }
+    }
+}
